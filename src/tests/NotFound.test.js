37a2d269073a1b8d82c40a6470dcd5c79132d0a6
LeoLacana <wilson17414@gmail.com>
2021-07-01 18:47:11 -0300

    requisito NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,22 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import NotFound from '../components/NotFound';
+
+describe('Teste o componente <About.js /', () => {
+  it('Teste se a página contém um heading h2 com o texto About Pokédex', () => {
+    const { container, getByText } = render(<NotFound />);
+    const h2NotFound = container.querySelector('h2');
+    const h2Text = getByText('Page requested not found');
+
+    expect(h2NotFound).toBeInTheDocument();
+    expect(h2Text).toBeInTheDocument('Page requested not found');
+  });
+
+  it('Teste se a página contém a imagem de uma Pokédex', () => {
+    const { getByAltText } = render(<NotFound />);
+    const imageNotFound = getByAltText(/Pikachu/i);
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(imageNotFound.src).toContain(url);
+  });
+});
