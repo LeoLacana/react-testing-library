cdf78176e3a5c9c1831088ff3658bfa8de83dcfd
LeoLacana <wilson17414@gmail.com>
2021-07-01 18:19:37 -0300

    requisito FavoritePokemons

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,11 @@
-test('', () => {});
+import React from 'react';
+import { render } from '@testing-library/react';
+import FavoritePokemons from '../components/FavoritePokemons';
+
+describe('Teste o componente <FavoritePokemons.js /', () => {
+  it('Teste se tem a mensagem No favorite pokemon found, se não tiver pokemons.', () => {
+    const { getByText } = render(<FavoritePokemons />);
+    const h2Pokedex = getByText('No favorite pokemon found');
+    expect(h2Pokedex).toBeInTheDocument();
+  });
+});
